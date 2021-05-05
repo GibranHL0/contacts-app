52a9a5ed28d6e28e535061e2943cb58012b9c0ef
Gibran <gibranhl2@gmail.com>
2021-05-05 14:41:37 -0500

    test to heroku

--- api/app.py
+++ api/app.py
@@ -1,21 +1,59 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 
 app = Flask(__name__)
-app.config['DATABASE_URI'] = Config.DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = Config.DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
-class UsersModel(db.Model):
-    __tablename__ = 'users'
+class Contacts(db.Model):
+    __tablename__ = 'contacts'
+    email = db.Column(db.String(), primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
+    company = db.Column(db.String(40), nullable=True)
+    phone = db.Column(db.Integer, nullable=True)
 
-    email = db.Column(db.String(),primary_key=True)
+    def __init__(self, name, last_name, company, phone, email):
+        self.name = name
+        self.last_name = last_name
+        self.company = company
+        self.phone = phone
+        self.email = email
 
-@app.route('/')
+@app.route('/contacts', methods= ['POST'])
 def hello():
-    return 'Hey Gibran!'
+    info = request.get_json()
+    name = info.get("name")
+    last_name = info.get("last_name")
+    company = info.get("company")
+    phone = info.get("phone")
+    email = info.get("email")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    new_contact = Contacts(name,last_name,company,phone,email)
+    db.session.add(new_contact)
+    db.session.commit()
+
+    response = make_response(
+        jsonify(
+            {'data': {
+                'email' : email,
+                'name': name,
+                'last_name': last_name,
+                'company': company,
+                'phone': phone,
+                },
+             'msg': 'Information recieved'
+            }
+        ),
+        200
+    )
+    response.headers['Content-Type'] = "application/json"
+
+    return response
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- api/config.py
+++ api/config.py
@@ -1,3 +1,2 @@
 class Config(object):
-    SECRET_KEY = '87083ff8aa02761c31877d8c872dda30247feb4ee8b493c20c8352b2b75e1032'
-    DATABASE_URI = 'postgres://wvuhngvtjkehor:87083ff8aa02761c31877d8c872dda30247feb4ee8b493c20c8352b2b75e1032@ec2-35-174-35-242.compute-1.amazonaws.com:5432/d3qq8f1rh6f8n'
+    DATABASE_URI = 'postgres://avclytlfaondze:dc6dde67d51e723244f932061728e7b9905b2c0aff20465e3cd54f0466df38ea@ec2-3-233-7-12.compute-1.amazonaws.com:5432/dblro19vhr293t'
