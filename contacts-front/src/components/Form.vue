f46264748f95422f0c60c7fec87cee8d803372ce
Gibran <gibranhl2@gmail.com>
2021-05-05 12:00:46 -0500

    form created and axios included

--- contacts-front/src/components/Form.vue
+++ contacts-front/src/components/Form.vue
@@ -1,97 +1,52 @@
 <template>
     <div key="Form" class="pl-20 pt-20">
-        <div class="flex items-center h-screen w-full bg-teal-lighter">
+        <div class="flex items-center h-screen w-full">
             <div class="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
-                <h1 class="block w-full text-center text-grey-darkest mb-6 text-2xl font-medium">
+                <h1 class="block w-full text-center text-black mb-6 text-2xl font-medium">
                     New Contact
                 </h1>
                 <form class="mb-4 md:flex md:flex-wrap md:justify-between" action="/" method="post">
-                    <div class="field-group mb-4 md:w-1/2">
-                        <label class="field-label" for="first_name">First Name</label>
-                        <input class="field md:mr-2" type="text" name="first_name" id="first_name">
+                    <div class="flex flex-col mb-4 md:w-1/2">
+                        <label class="font-medium text-lg text-gray-900 mb-2" for="first_name">
+                            Name *
+                        </label>
+                        <input class="border py-2 px-3 text-gray-900 md:mr-2" type="text" name="first_name" id="first_name" pattern="[a-zA-Z]{1,}" required>
                     </div>
-                    <div class="field-group mb-4 md:w-1/2">
-                        <label class="field-label md:ml-2" for="last_name">Last Name</label>
-                        <input class="field md:ml-2" type="text" name="last_name" id="last_name">
+                    <div class="flex flex-col mb-4 md:w-1/2">
+                        <label class="font-medium text-lg text-gray-900 mb-2 md:ml-2" for="last_name">
+                            Last Name *
+                        </label>
+                        <input class="border py-2 px-3 text-gray-900 md:ml-2" type="text" name="last_name" id="last_name" pattern="[a-zA-Z]{1,}" required>
                     </div>
-                    <div class="field-group mb-4 md:w-full">
-                        <label class="field-label" for="email">Email</label>
-                        <input class="field" type="email" name="email" id="email">
+                    <div class="flex flex-col mb-4 md:w-full">
+                        <label class="font-medium text-lg text-gray-900 mb-2" for="company">
+                            Company
+                        </label>
+                        <input class="border py-2 px-3 text-gray-900" type="text" name="company" id="company">
                     </div>
-                    <div class="field-group mb-6 md:w-full">
-                        <label class="field-label" for="password">Password</label>
-                        <input class="field" type="password" name="password" id="password">
+                    <div class="flex flex-col mb-4 md:w-full">
+                        <label class="font-medium text-lg text-gray-900 mb-2" for="phone">
+                            Phone
+                        </label>
+                        <input class="border py-2 px-3 text-gray-900" type="number" name="phone" id="phone">
                     </div>
-                    <button class="btn btn-teal mx-auto" type="submit">Create Account</button>
+                    <div class="flex flex-col mb-4 md:w-full">
+                        <label class="font-medium text-lg text-gray-900 mb-2" for="email">
+                            Email *
+                        </label>
+                        <input class="border py-2 px-3 text-gray-900" type="email" name="email" id="email" required>
+                        <p class="text-sm py-4">* Required</p>
+                    </div>
+                    <button class="bg-black hover:bg-red-400 mx-auto text-white px-2 py-2 rounded-md" type="submit">Create Account</button>
                 </form>
-                <a class="link link-grey w-full text-center" href="/login">Already have an account?</a>
+                <a class=" block no-underline text-sm text-gray-900 w-full text-center" href="/login">Cancel</a>
             </div>
         </div>
-        <!-- <div key="inner" class="bg-white w-2/6">
-            <div key="title" class="text-center bg-red-700 text-white">
-                <p class=" text-2xl font-medium">New Contact</p>
-            </div>
-            <div key="square" class="table pl-3 pt-2">
-                <div key="in-form" class="table-row-group">
-                    <div key="second-row" class="table-row">
-                        <div key="name-title" class="table-cell">
-                            <p class="text-lg">Name *</p>
-                        </div>
-                        <div key="name-value" class="table-cell pl-2">
-                            <input id="name-val" type="text" 
-                            class="border-black border-2 rounded-md">
-                        </div>
-                    </div>
-                    <div key="third-row" class="table-row">
-                        <div key="last-title" class="table-cell">
-                            <p>Last Name *</p>
-                        </div>
-                        <div key="last-value" class="table-cell">
-                            <input type="text" placeholder="Hola">
-                        </div>
-                    </div>
-                    <div key="fourth-row" class="table-row">
-                        <div key="company-title" class="table-cell">
-                            <p>Company</p>
-                        </div>
-                        <div key="company-value" class="table-cell">
-                            <input type="text" placeholder="Hola">
-                        </div>
-                    </div>
-                    <div key="fifth-row" class="table-row">
-                        <div key="phone-title" class="table-cell">
-                            <p>Phone *</p>
-                        </div>
-                        <div key="phone-value" class="table-cell">
-                            <input type="text" placeholder="Hola">
-                        </div>
-                    </div>
-                    <div key="sixth-row" class="table-row">
-                        <div key="email-title" class="table-cell">
-                            <p>Email *</p>
-                        </div>
-                        <div key="email-value" class="table-cell">
-                            <input type="text" placeholder="Hola">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div key="required" class="table-cell">
-                <p>* Required</p>
-            </div>
-            <div key="actions" class="space-x-2">
-                <div key="cancel-button" class="inline-block">
-                    <button class="bg-white">Cancel</button>
-                </div>
-                <div key="accept-button" class="inline-block">
-                    <button class="bg-white">Create</button>
-                </div>
-            </div>
-        </div> -->
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'Form',
   props: {
@@ -99,6 +54,14 @@ export default {
   return: {
   },
   methods: {
+      sendInfo(){
+          console.log()
+      },
+      created(){
+        const article = {title: "Vue"};
+        axios.post("https://reqres.in/api/articles", article)
+        .then(response => this.articleId = response.data.id);
+      },
   }
 }
 </script>
